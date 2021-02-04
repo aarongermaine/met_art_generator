eb849487813b020fc6b4925e9a5a7c184d1a1878
Dom Rafiti <dom.rafiti@gmail.com>
2021-02-03 21:20:57 -0700

    adding main page psuedo code to script.js file

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+/** ----------------PSUEDO CODE FOR MAIN PAGE-----------------
+
+Once the page loads i enter my keyword into the search box and hit submit
+-- If no results are found a message is presented on the web page
+
+My search is passed into the Met API search
+-- search criteria is passed into initial MET API and the object IDs are stored into an array
+-- a random object ID is selected and passed into the second MET API and the detailed results are stored into an object
+---- Date
+---- Location in museum
+---- Period Type
+---- Image
+---- Name of Artist
+---- Title of piece/work
+---- Rights and Reproduction
+
+-- API Parameters:
+----HAS IMAGE
+
+
+The results are displayed on the screen
+
+
+
+
+ */
